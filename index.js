dc2605ba798375578bc346d6fcf3632c2a95884f
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-12-20 13:37:35 +0100

    comment on index

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// importerer required moduler og routere herunder
 import express from 'express';
 import { CategoryRouter } from './Routes/category.router.js';
 import { ProductRouter } from './Routes/product.router.js';
 import { SyncRouter } from './Routes/sync.router.js';
 
+// Definer porten for serveren at lytte på
 const port = process.env.PORT || 4000;
+// Opretter express app
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 
 // Enable JSON parsing middleware
